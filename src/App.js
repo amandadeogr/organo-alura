04fb9915e244e02a27521328f189906cc6ab1a79
amandadeog <amandadeog@gmail.com>
2022-10-08 20:33:41 -0300

    fix: Corrigindo bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
-import Times from './components/Times';
+import Time from './components/Time';
 
 function App() {
 
@@ -9,59 +9,60 @@ function App() {
     {
       nome: 'Programação',
       corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9',
-    } ,
-
+      corSecundaria: '#D9F7E9'
+    },
     {
       nome: 'Front-End',
       corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF',
+      corSecundaria: '#E8F8FF'
     },
-
     {
-        nome: 'Data Sciense',
-        corPrimaria: '#A6D157',
-        corSecundaria: '#F0F8E2',
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
     },
-
     {
-        nome: 'Devops',
-        corPrimaria: '#E06B69',
-        corSecundaria: '#FDE7E8',
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
     },
-
     {
-        nome: 'UX e Design',
-        corPrimaria: '#D86EBF',
-        corSecundaria: '#FAE95F5',
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
     },
-
     {
-        nome: 'Mobile',
-        corPrimaria: '#FEBA05',
-        corSecundaria: '#FFF5D9',
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
     },
-
     {
-        nome: 'Inovação e Gestão',
-        corPrimaria: '#FF8A29',
-        corSecundaria: '#FFEEDF',
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
     }
   ]
 
-  const [colaboradores, setColaboradores] = useState([]);
+  const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador);
-    setColaboradores({...colaboradores, colaborador})
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
   }
 
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      
-      {times.map(time => <Times key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
+
     </div>
   );
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,74 +1,67 @@
-import { useState } from "react";
-import Botao from "../botao";
-import CampoTexto from "../CampoTexto";
-import ListaSuspensa from "../ListaSuspensa";
-import "./Formulario.css";
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
 
 const Formulario = (props) => {
 
-  const times = [
-    'Programação',
-    'Front-end',
-    'Data Science',
-    'Devops',
-    'UX e UI',
-    'Mobile',
-    'Inovação e Gestão'
-  ]
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
 
-  const aoSalvar = (evento) => {
-    evento.preventDefault();
-    props.aoColaboradorCadastrado({
-      nome,
-      cargo,
-      imagem,
-      time
-    })
-  }
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
+    }
 
-  const [nome, setNome] = useState('');
-  const [cargo, setCargo] = useState('');
-  const [imagem, setImagem] = useState('');
-  const [time, setTime] = useState('')
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto 
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome" 
+                    valor={nome}
+                    aoAlterar={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo" 
+                    valor={cargo}
+                    aoAlterar={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem" 
+                    valor={imagem}
+                    aoAlterar={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time" 
+                    itens={props.times}
+                    valor={time}
+                    aoAlterar={valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
 
-  return (
-    <section className="formulario">
-      <form onSubmit={aoSalvar}>
-         <h2>Preencha os dados para criar os nossos cards</h2>
-        <CampoTexto 
-          obrigatorio={true} 
-          label="Nome" 
-          placeholder="Digite o seu nome" 
-          valor={nome}
-          aoAlterar={valor => setNome(valor)}
-        />
-
-        <CampoTexto 
-          obrigatorio={true}  
-          label="Cargo" 
-          placeholder="Digite o seu cargo"
-          valor={cargo}
-          aoAlterar={valor => setCargo(valor)}
-        />
-
-        <CampoTexto 
-          label="Imagem" 
-          placeholder="Digite o endereço da imagem" 
-          valor={imagem}
-          aoAlterar={valor => setImagem(valor)}
-        />
-
-        <ListaSuspensa 
-          obrigatorio={true}  
-          label="Times" 
-          itens={times}
-          valor={time}
-          aoAlterar={valor => setTime(valor)}
-        />
-        <Botao>Criar Card</Botao>
-      </form>
-    </section>
-  );
-};
-
-export default Formulario;
+export default Formulario
