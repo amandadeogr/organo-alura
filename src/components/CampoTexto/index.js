7781154b61d17a4990bab585d4c595b9a9bf9cd4
amandadeog <amandadeog@gmail.com>
2022-10-08 21:28:50 -0300

    finalização do projeto

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -9,7 +9,7 @@ const CampoTexto = (props) => {
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input value={props.value} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} />
+      <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} />
     </div>
   );
 };
