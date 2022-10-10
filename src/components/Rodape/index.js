831e44d411015d9f0b40e8a59a0a96ec06313ef4
amandadeog <amandadeog@gmail.com>
2022-10-10 09:50:32 -0300

    Finalizando rodapé

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -1,37 +1,37 @@
-import './Rodape.css'
+//src/componentes/Rodape/index.js
+
+import './rodape.css'
 
 const Rodape = () => {
-   return (
-      <div className='rodape'>
-         <div className='icones'>
+    return (<footer className="footer">
+        <section>
             <ul>
-               <li>
-                  <a href='/'>
-                     <img src='/imagens/fb.png' alt='' />
-                  </a>
-               </li>
-               <li>
-                  <a href='/'>
-                     <img src='/imagens/tw.png' alt='' />
-                  </a>
-               </li>
-               <li>
-                  <a href='/'>
-                     <img src='/imagens/ig.png' alt='' />
-                  </a>
-               </li>
+                <li>
+                    <a href="facebook.com" target="_blank">
+                        <img src="/imagens/facebook.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="twitter.com" target="_blank">
+                        <img src="/imagens/twitter.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="instagram.com" target="_blank">
+                        <img src="/imagens/instagram.png" alt="" />
+                    </a>
+                </li>
             </ul>
-         </div>
-
-         <div className='logo'>
-            <img src='/imagens/logo.png' alt='' />
-         </div>
-
-         <div className='direitos'>
-            <p>Desenvolvido por Alura</p>
-         </div>
-      </div>
-   )
+        </section>
+        <section>
+            <img src="/imagens/logo.png" alt="" />
+        </section>
+        <section>
+            <p>
+                Desenvolvido por Alura.
+            </p>
+        </section>
+    </footer>)
 }
 
 export default Rodape
